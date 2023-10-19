55ebe69f58eac68d92bddfbf5a11fe6b4d0ec3b6
Ibrahim-Shaaban <hemaabdelnaby325@gmail.com>
2023-10-20 00:10:29 +0300

    check if room is available

---
+++ app/services/room/check_available_for_reservation_service.rb
@@ -0,0 +1,42 @@
+class Room::CheckAvailableForReservationService
+    
+    def initialize(room:, start_date:, end_date:)
+        @room = room
+        @start_date = start_date
+        @end_date = end_date
+    end
+
+    def call 
+        check_room_is_available?
+    end
+
+    private
+
+    def check_room_is_available?
+        # Find overlapping reservations
+        overlapping_reservations = @room.reservations.where('start_date <= ? AND end_date >= ?', @end_date, @start_date)
+
+        # Check if there are any reservations that conflict with the provided dates
+        if overlapping_reservations.any?
+            return false
+        end
+
+        true
+
+        # # Check for potential gaps between reservations
+        # reservations = @room.reservations.order(:start_date)
+
+        # return true if reservations.empty?  # If there are no existing reservations
+
+        # last_end_date = reservations.first.start_date
+        # reservations.each do |reservation|
+        #     if @start_date > last_end_date && @end_date < reservation.start_date
+        #         return true
+        #     end
+        #     last_end_date = reservation.end_date
+        # end
+
+        # false
+        
+    end
+end
